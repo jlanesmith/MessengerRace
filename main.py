dcfd8f0d4054ba867b47daf6900d584c88449c23
Jonathan Lane-Smith <jlanesmith1@gmail.com>
2020-06-28 18:15:11 -0400

    Update description

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 #!/c/Users/jlane/AppData/Local/Programs/Python/Python38-32/python
 
 '''
-This script creates a CSV file that includes the total number of messages you have with every Facebook conversation,
-over time. From here, a bar chart race can be created to visualize your conversations. For more information, see the guide
-at https://towardsdatascience.com/step-by-step-tutorial-create-a-bar-chart-race-animation-da7d5fcd7079.
-Flourish is a program which can create Bar Chart races, given a CSV file, and that's exactly what happens here.
+This script creates a CSV file which consists of the total number of messages you have for every Facebook conversation, 
+everyday, over time. From here, a bar chart race can be created to visualize your conversations. For more information, 
+see the guide at https://towardsdatascience.com/step-by-step-tutorial-create-a-bar-chart-race-animation-da7d5fcd7079. 
+Flourish is a program which can create bar chart races, given a CSV file.
+
+To use this script, you must first download your messenger information from Facebook. This script does not make any network calls.
 '''
 
 import os, json, csv
