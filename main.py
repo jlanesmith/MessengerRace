e148210d47f9281a729cb0aede9fcec69d1ab95b
Jonathan Lane-Smith <jonathan_lanesmith@apple.com>
2023-11-29 14:01:24 -0800

    Make script compatible with Mac too

--- main.py
+++ main.py
@@ -12,11 +12,14 @@ To use this script, you must first download your messenger information from Face
 import os, json, csv
 from datetime import datetime, timedelta
 
-folders = os.listdir("C://Users/jlane/Desktop/Untracked Files/Facebook Downloads/messages/inbox")
+# top_directory = "C://Users/jlane/Desktop/Untracked Files/Facebook Downloads"
+top_directory = "/Users/jonathanlanesmith/Downloads/your_activity_across_facebook 2"
+
+folders = os.listdir(top_directory + "/messages/inbox")
 
 groupByWeek = True # Whether to have a column for each week, vs everyday
 isMovingAverage = True # Whether to implement a moving average
-movingAveCount = 104 # How many columns to include in calculating the moving average
+movingAveCount = 52 # How many columns to include in calculating the moving average
 withGroupChats = True # Whether to include group chats
 
 globalStartDate = None # The date of the earliest message out of every conversation
@@ -38,7 +41,7 @@ def parseConvo(data):
     # If messageCount is 10000, there will be another message_#.json file
     messageFileNum = 2 # The number of the next message_#.json file, if it is read
     while messageCount >= 10000:
-        with open(f'C://Users/jlane/Desktop/Untracked Files/Facebook Downloads/messages/inbox/{folder}/message_{messageFileNum}.json') as json_file:
+        with open(f'{top_directory}/messages/inbox/{folder}/message_{messageFileNum}.json') as json_file:
             newData = json.load(json_file) # Load the data
             rawMessages = rawMessages + newData['messages'] # Put the data all together
             messageCount = len(newData['messages']) # Prepare to check if there's another message_#.json file
@@ -73,10 +76,11 @@ def parseConvo(data):
 
 # For each conversation
 for folder in folders:
-    with open(f'C://Users/jlane/Desktop/Untracked Files/Facebook Downloads/messages/inbox/{folder}/message_1.json') as json_file:
-        data = json.load(json_file) # Load the data
-        if (len(data['participants']) == 2) or withGroupChats: # Logic for including group chats or not
-            datesAndTotals.append(parseConvo(data))
+    if folder != ".DS_Store":
+        with open(f'{top_directory}/messages/inbox/{folder}/message_1.json') as json_file:
+            data = json.load(json_file) # Load the data
+            if (len(data['participants']) == 2) or withGroupChats: # Logic for including group chats or not
+                datesAndTotals.append(parseConvo(data))
 
 # Make first row of CSV which is just dates
 iterDate = globalStartDate
